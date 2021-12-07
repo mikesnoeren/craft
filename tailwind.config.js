9d539ff43ff4e6b4f5a5ddba8f64a832bd29a84f
mikesnoeren <snoeren.mike@gmail.com>
2021-12-07 23:42:15 +0100

    Fix tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,19 @@
 module.exports = {
-  purge: [
-    './templates/**/*.html',
-    './templates/**/*.twig',
-    './src/**/*.js',
-  ],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    mode: 'jit',
+    purge: [
+        './templates/**/*.html',
+        './templates/**/*.twig',
+        './src/**/*.js',
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+        container: {
+            center: true,
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
